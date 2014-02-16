a346bdcce2ef24770cd443828efed149693bd4d1
dogmaan <dogmaan@gmail.com>
2014-02-16 23:34:01 +0000

    sort of starting to work ish

--- web/js/app.js
+++ web/js/app.js
@@ -2,26 +2,17 @@
 
 
 // Declare app level module which depends on filters, and services
-var tclone = angular.module('tclone', [
-    'ngRoute',
-    'ui.bootstrap',
-    'tclone.filters',
-    'tclone.services',
-    'tclone.directives',
-    'tclone.controllers'
-]);
-
-tclone.config(['$routeProvider', function ($routeProvider) {
-    $routeProvider.
-        when('/about', {
-            templateUrl: 'partials/about.html',
-            controller: 'AboutCtrl'
-        }).
-        when('/test', {
-            templateUrl: 'partials/test.html',
-            controller: 'TestCtrl'
-        }).
-        otherwise({
-            redirectTo: '/about'
-        })
+angular.module('tclone', [
+  'ngRoute',
+  'tclone.filters',
+  'tclone.services',
+  'tclone.directives',
+  'tclone.controllers'
+]).
+config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
+  $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
+  $routeProvider.when('/about', {templateUrl: 'partials/about.html', controller: 'AboutController'});
+  $routeProvider.when('/test', {templateUrl: 'partials/test.html', controller: 'TestController'});
+  $routeProvider.otherwise({redirectTo: '/view1'});
 }]);
--- web/js/controllers.js
+++ web/js/controllers.js
@@ -1,25 +1,29 @@
 'use strict';
-var tclone = angular.module('tclone', []);
 
-tclone.controller('NavbarCtrl', ['$scope', function ($scope) {
-    $scope.navs = [
-        {
-            'name': 'about',
-            'href': '#/about'
-        },
-        {
-            'name': 'test',
-            'href': '#/test'
-        }
-    ];
-}]);
+/* Controllers */
 
-tclone.controller('AboutCtrl', ['$scope', function ($scope) {
-    $scope.about_message = "this is the test about message";
-}]);
+angular.module('tclone.controllers', []).
+    controller('MyCtrl1', [function () {
 
-tclone.controller('TestCtrl', ['$scope', function ($scope) {
+    }])
+    .controller('MyCtrl2', [function () {
 
-}]);
-
-
+    }])
+    .controller('NavBarController', ['$scope', function ($scope) {
+        $scope.nav_links = [
+            {
+                'name': 'about',
+                'url': '#/about'
+            },
+            {
+                'name': 'test',
+                'url': '#/test'
+            }
+        ];
+    }])
+    .controller('AboutController', ['$scope', function ($scope) {
+        $scope.about_message = "Twitter Clone";
+    }])
+    .controller('TestController', ['$scope', function ($scope) {
+        $scope.test_message = "Test page";
+    }]);
