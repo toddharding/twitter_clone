4c9fead287e4c324b648be26d58d169b3edcaa32
dogmaan <dogmaan@gmail.com>
2014-02-25 08:46:43 +0000

    Added user url selection

--- web/js/controllers.js
+++ web/js/controllers.js
@@ -3,20 +3,18 @@
 /* Controllers */
 
 angular.module('tclone.controllers', [])
-    .controller('NavBarController', ['$scope', 'AuthenticationService','Globals', function ($scope, AuthenticationService, Globals) {
+    .controller('NavBarController', ['$scope', 'AuthenticationService', 'Globals', function ($scope, AuthenticationService, Globals) {
         $scope.authService = AuthenticationService;
         $scope.globVars = Globals;
         $scope._user = null;
         $scope.isUserLoggedIn = false;
-        $scope.$watch('globVars.getUser()', function(user){
+        $scope.$watch('globVars.getUser()', function (user) {
             console.log("Watch User: ", user);
             $scope._user = user;
-            if($scope._user != null)
-            {
+            if ($scope._user != null) {
                 $scope.isUserLoggedIn = true
             }
-            else
-            {
+            else {
                 $scope.isUserLoggedIn = false;
             }
         })
@@ -52,21 +50,21 @@ angular.module('tclone.controllers', [])
     .controller('TestController', ['$scope', function ($scope) {
         $scope.test_message = "Test page";
     }])
-    .controller('LoginFormController', ['$scope', '$http','AuthenticationService', 'Auth', 'Globals', '$location', function($scope, $http, AuthenticationService, Auth, Globals, $location){
-        $scope.user = {'username': '', 'password':''};
+    .controller('LoginFormController', ['$scope', '$http', 'AuthenticationService', 'Auth', 'Globals', '$location', function ($scope, $http, AuthenticationService, Auth, Globals, $location) {
+        $scope.user = {'username': '', 'password': ''};
         $scope.test_message = ""
-        $scope.login = function(){
+        $scope.login = function () {
             Auth.setCredentials($scope.user.username, $scope.user.password);
             $scope.test_message = "logging in";
             $http.post('auth').
-                success(function(data){
+                success(function (data) {
                     $scope.test_message = "logged in";
                     $scope.user.id = data.id;
                     AuthenticationService.login({username: $scope.user.username, id: $scope.user.id});
                     Globals.setUser({name: $scope.user.username, id: $scope.user.id});
                     $location.path("/app");
                 }).
-                error(function(data){
+                error(function (data) {
                     $scope.test_message = "Incorrect Username or Password";
                     $scope.user = null;
                     Auth.clearCredentials();
@@ -74,8 +72,8 @@ angular.module('tclone.controllers', [])
                 });
         };
     }])
-    .controller('LogoutController', ['$scope', '$http', '$location','AuthenticationService', 'Auth', 'Globals', function($scope, $http, $location, AuthenticationService, Auth, Globals) {
-        $scope.logout = function(){
+    .controller('LogoutController', ['$scope', '$http', '$location', 'AuthenticationService', 'Auth', 'Globals', function ($scope, $http, $location, AuthenticationService, Auth, Globals) {
+        $scope.logout = function () {
             AuthenticationService.logout();
             Auth.clearCredentials();
             $location.path("/login");
@@ -86,42 +84,52 @@ angular.module('tclone.controllers', [])
         $scope.test_message = "App page";
         $scope.user = SessionService.currentUser;
     }])
-    .controller('HomeUserDetailsController', ['$scope', '$http', '$location','AuthenticationService', 'Auth', 'Globals', function($scope, $http, $location, AuthenticationService, Auth, Globals) {
+    .controller('HomeUserDetailsController', ['$scope', '$http', '$location', 'AuthenticationService', 'Auth', 'Globals', function ($scope, $http, $location, AuthenticationService, Auth, Globals) {
         $scope.user = {};
         $http.get('/user/' + Globals.getUser().id)
-            .success(function(data){
+            .success(function (data) {
                 $scope.user = data;
             })
-            .error(function(){
+            .error(function () {
                 $scope.user = null;
             });
+        $scope.tweet = "";
     }])
-    .controller('HomeFeedController', ['$scope', '$http', '$location','AuthenticationService', 'Auth', 'Globals', function($scope, $http, $location, AuthenticationService, Auth, Globals) {
+    .controller('HomeFeedController', ['$scope', '$http', '$location', 'AuthenticationService', 'Auth', 'Globals', function ($scope, $http, $location, AuthenticationService, Auth, Globals) {
         $scope.tweets = {};
         $scope.title = "Feed";
     }])
-    .controller('PublicUserProfileController', ['$scope', '$http', '$location','AuthenticationService', 'Auth', 'Globals', '$stateParams',
-        function($scope, $http, $location, AuthenticationService, Auth, Globals, $stateParams) {
+    .controller('PublicUserProfileController', ['$scope', '$http', '$location', 'AuthenticationService', 'Auth', 'Globals', '$stateParams',
+        function ($scope, $http, $location, AuthenticationService, Auth, Globals, $stateParams) {
             $scope.selectedUser = {};
             $http.get('/user/' + $stateParams.username)
-                .success(function(data){
+                .success(function (data) {
                     $scope.selectedUser = data;
                 })
-                .error(function(){
+                .error(function () {
                     $scope.selectedUser = null;
                 });
         }])
-    .controller('SelectedUserDetailsController', ['$scope', '$http', '$location','AuthenticationService', 'Auth', 'Globals', '$stateParams',
-        function($scope, $http, $location, AuthenticationService, Auth, Globals, $stateParams) {
-        $scope.selectedUser = {};
-        $http.get('/user/' + $stateParams.username)
-            .success(function(data){
-                $scope.selectedUser = data;
-            })
-            .error(function(){
-                $scope.selectedUser = null;
-            });
-    }]).controller('SelectedUserTweetsController', ['$scope', '$http', '$location','AuthenticationService', 'Auth', 'Globals', function($scope, $http, $location, AuthenticationService, Auth, Globals) {
-        $scope.tweets = {};
-        $scope.title = "Feed";
-    }]);
+    .controller('SelectedUserDetailsController', ['$scope', '$http', '$location', 'AuthenticationService', 'Auth', 'Globals', '$stateParams',
+        function ($scope, $http, $location, AuthenticationService, Auth, Globals, $stateParams) {
+            $scope.selectedUser = {};
+            $http.get('/user/' + $stateParams.username)
+                .success(function (data) {
+                    $scope.selectedUser = data;
+                })
+                .error(function () {
+                    $scope.selectedUser = null;
+                });
+        }])
+    .controller('SelectedUserTweetsController', ['$scope', '$http', '$location', 'AuthenticationService', 'Auth', 'Globals', '$stateParams',
+        function ($scope, $http, $location, AuthenticationService, Auth, Globals, $stateParams) {
+            $scope.tweets = {};
+            $scope.title = "Feed";
+            $http.get('/tweet/' + $stateParams.username)
+                .success(function (data) {
+                    $scope.tweets = data;
+                })
+                .error(function () {
+                    $scope.tweets = null;
+                });
+        }]);
